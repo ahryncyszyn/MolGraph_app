5b9e4e8b331c2609e3a30c807c5acac25e9b8b2c
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-03-12 17:15:39 +0100

    added the buttons for calculating molecular mass and visualization

--- calculations_window.py
+++ calculations_window.py
@@ -1,55 +1,91 @@
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QComboBox, QPushButton, QWidget, QHBoxLayout, QFileDialog, QLabel
+from PyQt5.QtWidgets import \
+    QMainWindow, QGridLayout, QComboBox, QPushButton, QWidget, QSizePolicy, QLabel
 import PyQt5.QtGui as qtg
-import sys
+from PyQt5.QtCore import Qt
 import reading_xyz as xyz
 
 class CalculationsWindow(QMainWindow):
-    def __init__(self, results):
+    def __init__(self, results, main_window):
         super().__init__()
-
-        self.setWindowTitle("Calculations")
-        self.setGeometry(100, 100, 600, 300)
-
-        #sets a central widget
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        #sets layout
-        self.layout = QGridLayout()
-        self.central_widget.setLayout(self.layout)
+        self.main_window = main_window
 
         #create a label
         my_label = QLabel("Select a molecule:")
-        #change font size of label
-        my_label.setFont(qtg.QFont('Helvetica', 18))
-        self.layout.addWidget(my_label)
+        my_label.setAlignment(Qt.AlignCenter)
+        my_label.setFont(qtg.QFont('SF Pro', 25))
 
         #combo box for choosing a molecule
         self.combo_box = QComboBox(self)
+        self.combo_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.combo_box.setMinimumWidth(200)
+        self.combo_box.setMinimumHeight(50)
         for molecule_name, molecule_object in results.items():
             self.combo_box.addItem(molecule_name, molecule_object)
-        self.layout.addWidget(self.combo_box)
 
-        self.mol_choice = self.combo_box.currentData()
+        #combo box for choosing the precision
+        self.precision_combo_box = QComboBox(self)
+        self.precision_combo_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.precision_combo_box.setMinimumWidth(200)
+        self.precision_combo_box.setMinimumHeight(50)
+        for i in range(11):
+            self.precision_combo_box.addItem(f'Precision: {i} decimal places', i)
 
         #button for calculating molecular mass
         calculate_mass_button = QPushButton("Calculate Molecular Mass")
-        print(f'you selected {self.mol_choice}')
+        calculate_mass_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         calculate_mass_button.clicked.connect(self.calculate_mass)
-        self.layout.addWidget(calculate_mass_button)
+
+        #add a label to display the result
+        self.mass_result_label = QLabel("Result will appear here")
+        self.mass_result_label.setAlignment(Qt.AlignCenter)
+        self.mass_result_label.setFont(qtg.QFont('SF Pro', 25)) 
 
         #button for visualizing the molecule
         visualize_button = QPushButton("Visualize")
-        #visualize_button.clicked.connect(self.visualize)
-        self.layout.addWidget(visualize_button)
+        visualize_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        visualize_button.clicked.connect(self.visualize)
+
+        #button to go back to the main window
+        go_back_button = QPushButton("Go back to menu")
+        go_back_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        go_back_button.clicked.connect(self.go_back)
+
+        #sets layout
+        self.layout = QGridLayout()
+        self.layout.addWidget(my_label, 0, 0, Qt.AlignCenter)
+        self.layout.addWidget(self.combo_box, 0, 1, Qt.AlignCenter)
+        self.layout.addWidget(self.precision_combo_box, 1, 0, Qt.AlignCenter)
+        self.layout.addWidget(calculate_mass_button, 1, 1, Qt.AlignCenter)
+        self.layout.addWidget(visualize_button, 3, 0, 1, 2, Qt.AlignCenter)
+        self.layout.addWidget(go_back_button, 4, 0, 1, 2, Qt.AlignCenter)
+
+        #sets a central widget
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+        self.setWindowTitle("Calculations")
+        self.setGeometry(100, 100, 600, 250)
 
     def calculate_mass(self):
-        mass = self.mol_choice.calculate_mol_mass()
-        print(mass)
+        chosen_molecule_text = self.combo_box.currentText()
+        chosen_molecule_obj = self.combo_box.currentData()
+        precision = self.precision_combo_box.currentData()
+        mass = round(chosen_molecule_obj.calculate_mol_mass(), precision)
+        self.layout.addWidget(self.mass_result_label, 2, 0, 1, 2, Qt.AlignCenter)
+        self.mass_result_label.setText(f'Molecular mass of {chosen_molecule_text} is {mass}')
 
 
-    def visualized(self):
-        pass
+    def visualize(self):
+        try:
+            chosen_molecule_obj = self.combo_box.currentData()
+            chosen_molecule_obj.plot_molecule()
+        except Exception as e:
+            print("An eror occured during visualization: ", e)
+
+    def go_back(self):
+        self.close()
+        self.main_window.show()
+
 
 
             
